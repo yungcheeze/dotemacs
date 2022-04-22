96c71d5eab6d58a0164e91a213ed974b50fe66ad
Ucizi Mafeni <ucizim@gmail.com>
2022-04-22 09:03:34 +0100

    just getting started

---
+++ early-init.el
@@ -0,0 +1,75 @@
+;;; early-init.el --- Early initialization -*- lexical-binding: t; no-byte-compile: t -*-
+
+;; Author: Andrey Listopadov
+;; Keywords: Emacs configuration
+;; Homepage: https://gitlab.com/andreyorst/dotfiles.git
+
+;;; Commentary:
+;; Emacs early initialization configuration.
+
+;;; Code:
+
+(setq
+ gc-cons-threshold most-positive-fixnum
+ read-process-output-max (* 1024 1024 4) ; 4mb
+ inhibit-compacting-font-caches t
+ message-log-max 16384
+ package-enable-at-startup nil
+ load-prefer-newer noninteractive)
+
+(setq-default
+ default-frame-alist '((width . 170)
+                       (height . 56)
+                       (tool-bar-lines . 0)
+                       (bottom-divider-width . 0)
+                       (right-divider-width . 1))
+ initial-frame-alist initial-frame-alist
+ frame-inhibit-implied-resize t
+ x-gtk-resize-child-frames 'resize-mode
+ fringe-indicator-alist (assq-delete-all 'truncation fringe-indicator-alist))
+
+(unless (or (daemonp) noninteractive)
+  (let ((restore-file-name-handler-alist file-name-handler-alist))
+    (setq-default file-name-handler-alist nil)
+    (defun restore-file-handler-alist ()
+      (setq file-name-handler-alist
+            (delete-dups (append file-name-handler-alist
+                                 restore-file-name-handler-alist)))))
+
+  (add-hook 'emacs-startup-hook #'restore-file-handler-alist 101)
+
+  (when (fboundp #'tool-bar-mode)
+    (tool-bar-mode -1))
+
+  (when (fboundp #'scroll-bar-mode)
+    (scroll-bar-mode -1)))
+
+(when (featurep 'native-compile)
+  (defvar native-comp-deferred-compilation)
+  (setq native-comp-deferred-compilation t)
+  (defvar native-comp-async-report-warnings-errors)
+  (setq native-comp-async-report-warnings-errors nil))
+
+;;; Straight
+
+(defvar straight-process-buffer)
+(setq-default straight-process-buffer " *straight-process*")
+
+(defvar straight-build-dir)
+(setq straight-build-dir (format "build-%s" emacs-version))
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(provide 'early-init)
+;;; early-init.el ends here
---
+++ init.el
@@ -0,0 +1,86 @@
+;;; Early init support for older Emacs
+
+(unless (featurep 'early-init)
+  (load (expand-file-name "early-init" user-emacs-directory)))
+
+(require 'straight)
+(straight-use-package 'use-package)
+(require 'use-package)
+
+;; Packages
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
+
+(use-package xclip
+  :straight (xclip :type git :host github :repo "emacsmirror/xclip")
+  :config
+  (xclip-mode 1))
+
+;;; Helm 2.0 
+(use-package selectrum
+  :config
+  (selectrum-mode +1)
+  (setq completion-styles '(orderless))
+
+  ;; Persist history over Emacs restarts
+  (savehist-mode)
+
+  ;; Optional performance optimization
+  ;; by highlighting only the visible candidates.
+  (setq orderless-skip-highlighting (lambda () selectrum-is-active))
+  (setq selectrum-highlight-candidates-function #'orderless-highlight-matches))
+
+(use-package orderless)
+
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode))
+
+(use-package embark
+  :ensure t
+
+  :bind
+  (("M-." . embark-act)         ;; pick some comfortable binding
+   ("M-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t
+  :after (embark consult)
+  :demand t ; only necessary if you have the hook below
+  ;; if you want to have consult previews as you move around an
+  ;; auto-updating embark collect buffer
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package which-key
+  :config
+  (which-key-mode))
+
+
+;;; Setup Shit
+(setq backup-directory-alist
+      `(("." . ,(concat user-emacs-directory "backups"))))
+ 
+;;; Custom Functions
+;; Source: http://www.emacswiki.org/emacs-en/download/misc-cmds.el
+(defun revert-buffer-no-confirm ()
+    "Revert buffer without confirmation."
+    (interactive)
+    (revert-buffer :ignore-auto :noconfirm))
