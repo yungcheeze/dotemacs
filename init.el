d40107c94eca173eaf80bee7f26f6c0992a20727
Ucizi Mafeni <ucizim@gmail.com>
2022-06-07 13:24:39 +0100

    add treesitter

--- init.el
+++ init.el
@@ -54,6 +54,14 @@
 (use-package all-the-icons
   :defer t)
 
+(use-package tree-sitter
+  :after tree-sitter-langs
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs)
+
 (use-package general
   :ensure t
   :preface
