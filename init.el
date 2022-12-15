5f89d8b8be45d14d682f2b7b540a5c1f5f561861
Ucizi Mafeni <ucizim@gmail.com>
2022-12-15 11:40:11 +0000

    add bind to accept both ediff regions on 3-way-diff

--- init.el
+++ init.el
@@ -1026,7 +1026,16 @@ point reaches the beginning or end of the buffer, stop there."
   :custom
   (ediff-window-setup-function 'ediff-setup-windows-plain)
   (ediff-diff-options "-w")
-  (ediff-split-window-function 'split-window-horizontally))
+  (ediff-split-window-function 'split-window-horizontally)
+  :config
+  (defun ediff-copy-both-to-C ()
+    (interactive)
+    (ediff-copy-diff ediff-current-difference nil 'C nil
+                     (concat
+                      (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                      (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+  (defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+  (add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map))
 
 (use-package auto-fill
   :straight nil
