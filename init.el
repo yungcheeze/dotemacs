b5a41e7616b6e75653b16179456936f156e17132
Ucizi Mafeni <ucizim@gmail.com>
2023-01-04 20:12:00 +0000

    finally! a magit quit that respects the previous window config

--- init.el
+++ init.el
@@ -288,7 +288,20 @@
   (magit-diff-refine-ignore-whitespace t)
   (magit-diff-refine-hunk 'all)
   :config
-  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
+  ;; http://whattheemacsd.com/setup-magit.el-01.html
+  ;; magit buffer that respects current window configuration
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+
+  (defun magit-quit-session ()
+    "Restores the previous window configuration and kills the magit buffer"
+    (interactive)
+    (kill-buffer)
+    (jump-to-register :magit-fullscreen))
+
+  (define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
 (use-package magit-delta
   :straight t
   :defer t
