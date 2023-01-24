d3780ca2785f8ec5e386e51e85285a58a5078116
Ucizi Mafeni <ucizim@gmail.com>
2023-01-24 10:31:57 +0000

    use bury buffer map on all magit buffers (not just status)

--- init.el
+++ init.el
@@ -302,7 +302,7 @@
     (jump-to-register :magit-fullscreen))
 
   (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
-  (define-key magit-status-mode-map (kbd "M-q") 'magit-mode-bury-buffer))
+  (define-key magit-mode-map (kbd "M-q") 'magit-mode-bury-buffer))
 (use-package magit-delta
   :straight t
   :defer t
